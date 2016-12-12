6bf489d7cf9e98060e786527f40dff87b7af1a25
David Jelenc <david.jelenc@fri.uni-lj.si>
2016-12-12 15:10:52 +0100

    Initial commit

---
+++ app/src/main/java/com/david/layouts/MainActivity.java
@@ -0,0 +1,29 @@
+package com.david.layouts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void addNumber(View view) {
+        final Button b = (Button) view;
+        final String newValue = textView.getText().toString() + b.getText().toString();
+        textView.setText(newValue);
+    }
+
+    public void clear(View view) {
+        textView.setText("");
+    }
+}
