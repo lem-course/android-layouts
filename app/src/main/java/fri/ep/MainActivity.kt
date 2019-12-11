8453f672fbfa12ed81e95e091ac1b4c26d504502
David Jelenc <david.jelenc@fri.uni-lj.si>
2019-12-11 11:37:04 +0100

    Updates the dependencies

--- app/src/main/java/fri/ep/MainActivity.kt
+++ app/src/main/java/fri/ep/MainActivity.kt
@@ -1,7 +1,7 @@
 package fri.ep
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
