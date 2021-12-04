e42cf80440301a46996dc41a7a6044d603d7ac7a
David Jelenc <david.jelenc@fri.uni-lj.si>
2021-12-04 23:34:49 +0100

    Updates project

--- app/src/main/java/fri/ep/MainActivity.kt
+++ app/src/main/java/fri/ep/MainActivity.kt
@@ -4,20 +4,23 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
-import kotlinx.android.synthetic.main.activity_main.*
+import fri.ep.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 
     fun addNumber(view: View) {
         val b = view as Button
-        val currentValue = textView.text.toString()
+        val currentValue = binding.textView.text.toString()
         val newValue = currentValue + b.text.toString()
 
-        textView.text = newValue
+        binding.textView.text = newValue
     }
 }
